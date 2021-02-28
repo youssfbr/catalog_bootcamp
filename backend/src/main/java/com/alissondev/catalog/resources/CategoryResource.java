72aaff381f940f4af48c29caa129910399cc50f8
Alisson Youssf <youssfbr@gmail.com>
2021-02-27 22:07:48 -0300

    CategoryResource

---
+++ backend/src/main/java/com/alissondev/catalog/resources/CategoryResource.java
@@ -0,0 +1,24 @@
+package com.alissondev.catalog.resources;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alissondev.catalog.entities.Category;
+
+@RestController
+@RequestMapping("/categories")
+public class CategoryResource {
+
+	@GetMapping
+	public ResponseEntity<List<Category>> findAll() {
+		List<Category> list = new ArrayList<>();
+		list.add(new Category(1L, "Books"));
+		list.add(new Category(2L, "Eletronics"));
+		return ResponseEntity.ok(list);
+	}
+}
