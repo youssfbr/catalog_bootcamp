650afca56b837926a27fef3b69b2b82d62f9e2ab
Alisson Youssf <youssfbr@gmail.com>
2021-03-01 20:27:57 -0300

    Update Category

--- backend/src/main/java/com/alissondev/catalog/resources/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/alissondev/catalog/resources/exceptions/ResourceExceptionHandler.java
@@ -9,13 +9,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.alissondev.catalog.services.exceptions.EntityNotFoundException;
+import com.alissondev.catalog.services.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
 
-	@ExceptionHandler(EntityNotFoundException.class)
-	public ResponseEntity<StandardError> entityNotFound(EntityNotFoundException e, HttpServletRequest request) {
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
 		HttpStatus status = HttpStatus.NOT_FOUND;
 		
 		StandardError err = new StandardError();
