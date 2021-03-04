7176ca7ea542be68621a618a89ff25157862f73d
Alisson Youssf <youssfbr@gmail.com>
2021-03-04 00:13:51 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,11 @@
-const App = () => {
+import './core/assets/styles/custom.scss';
+import './app.scss';
+import Navbar from './core/components/NavBar';
 
+const App = () => {
   return (
-    <div className="container mt-5">
-      <h1>App</h1>
-    </div>
+    <Navbar />
+   
   );
 }
 
